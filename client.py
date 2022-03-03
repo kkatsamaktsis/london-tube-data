f2d74e9ad8b444046a7d8fe2326592703d67a751
n68155kk <konstantinos.katsamaktsis@student.manchester.ac.uk>
2022-03-03 12:59:10 +0000

    Split long lines and remove password from code

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ data = ''
 with open('./train-network.json') as f:
     data = json.load(f)
 
-db = pymysql.connect(host="localhost", user="root", password="qhj38!@unSpY", database="tube")
+db = pymysql.connect(host="localhost", user="root", password="[PASSWORD]", database="tube")
 
 cursor = db.cursor()
 
@@ -16,7 +16,8 @@ while True:
         break
     elif text[0:len('station')] == 'station':
         station_name = text.split(' ')[1]
-        query = f"SELECT DISTINCT l.line_name FROM (line_stations AS l_s LEFT JOIN tube_lines AS l ON l_s.line_id = l.id  LEFT JOIN stations AS s ON l_s.station_id = s.id) WHERE s.station_name = '{station_name}';"
+        query = f"SELECT DISTINCT l.line_name FROM (line_stations AS l_s LEFT JOIN tube_lines AS l ON l_s.line_id = l.id" \
+                f"  LEFT JOIN stations AS s ON l_s.station_id = s.id) WHERE s.station_name = '{station_name}';"
         # print(query)
         cursor.execute(query)
         res = cursor.fetchall()
@@ -24,7 +25,8 @@ while True:
             print(l[0])
     elif text[0:len('line')] == 'line':
         line_name = text.split(' ')[1]
-        query = f"SELECT DISTINCT s.station_name FROM (line_stations AS l_s LEFT JOIN tube_lines AS l ON l_s.line_id = l.id LEFT JOIN stations AS s ON l_s.station_id = s.id) WHERE l.line_name = '{line_name}';"
+        query = f"SELECT DISTINCT s.station_name FROM (line_stations AS l_s LEFT JOIN tube_lines AS l ON l_s.line_id = l.id" \
+                f" LEFT JOIN stations AS s ON l_s.station_id = s.id) WHERE l.line_name = '{line_name}';"
         # print(query)
         cursor.execute(query)
         res = cursor.fetchall()
